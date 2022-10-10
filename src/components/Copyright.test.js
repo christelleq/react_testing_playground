ca5ff45634bed2fdbf68bdee7ffd35ccbd182077
Christelle Quilang <christellequilang@protonmail.com>
2022-10-10 16:41:41 +1300

    Change test

--- src/components/Copyright.test.js
+++ src/components/Copyright.test.js
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react'
 import Copyright from './Copyright'
 
 test('copyright date extends if current year is greater than initial year', () => {
+  jest.useFakeTimers('modern')
+  jest.setSystemTime(new Date('2023'))
   render(<Copyright />)
-  const dates = screen.getByText('20')
-  jest.useFakeTimers()
-  jest.setSystemTime(new Date('2023-01-01'))
-  expect(dates).toHaveTextContent('2022 - 2023')
+  const copyright = screen.getByText(/20/)
+  expect(copyright).toHaveTextContent(/2022 - 2023/)
 })
